b6af69a593da0268d062e1f06c70ad94dd070b12
Петровський Ігор <unlike041986@gmail.com>
2020-01-23 09:02:39 +0200

    work

--- down-web.py
+++ down-web.py
@@ -7,24 +7,5 @@ from urllib.request import urlopen
 html = urlopen("https://stepik.org/media/attachments/lesson/209719/2.html").read().decode('utf-8')
 
 s = str(html)
-s.split(" ")
-# print(s)
-# print('**************************************************************')
 
-for i in s:
-    print(i)
-
-
-# state = 0
-# ans = []
-# for c in s:
-#     if c == "<code>":
-#         state = 1
-#     if c == "/<code>":
-#         state = 0
-#     elif state == 0:
-#         ans.append(c)
-#
-# s = ''.join(ans)
-#
-# print(s)
+print(s)

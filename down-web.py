674f627c6101a1d8a4f2818a530ba65d5261cdf5
Петровський Ігор <unlike041986@gmail.com>
2020-01-23 13:58:06 +0200

    work

--- down-web.py
+++ down-web.py
@@ -6,14 +6,9 @@ from urllib.request import urlopen
 
 html = urlopen("https://stepik.org/media/attachments/lesson/209719/2.html").read().decode('utf-8')
 
-
 strName = str(html)
 
-
 print(strName.split())
-
-
-
 
 # state = 0
 # ans = []
